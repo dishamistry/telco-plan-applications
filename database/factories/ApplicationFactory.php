d50d61fc25dee3014fe2b5d9c0efdf4dd91e0261
dishamistry <mistrydisha6@gmail.com>
2025-08-29 16:23:32 +1000

    DB changed to SQLite and seed data added

--- database/factories/ApplicationFactory.php
+++ database/factories/ApplicationFactory.php
@@ -25,7 +25,7 @@ class ApplicationFactory extends Factory
             'plan_id' => Plan::factory(),
             'address_1' => $this->faker->sentence(1),
             'address_2' => rand(0, 1) > 0.8 ? $this->faker->sentence(1) : null,
-            'city' => $this->faker->sentence(1),
+            'city' => $this->faker->randomElement(['Sydney', 'Melbourne', 'Brisbane', 'Adelaide', 'Perth', 'Canberra', 'Hobart', 'Darwin', 'Geelong']),
             'state' => $this->faker->randomElement(['NSW', 'VIC', 'QLD', 'TAS', 'SA', 'WA', 'NT', 'ACT']),
             'postcode' => $this->faker->numerify('####'),
             'order_id' => null,
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,24 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Customer;
+use App\Models\Plan;
+use App\Models\Application;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->count(5)->create();
+        Customer::factory()->count(10)->create();
+        Plan::factory()->count(5)->create();
+        Application::factory()->count(10)->create();
     }
 }
